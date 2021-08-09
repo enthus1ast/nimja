230c4e79c8172907ca9e405a549697179d9119cc
David Krause <krause@biochem2.uni-frankfurt.de>
2021-08-09 20:11:16 +0200

    cleanup example; added UserContext to prologue example; removed gcsafe annotations
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- examples/prologue/serverPrologue.nim
+++ examples/prologue/serverPrologue.nim
@@ -8,30 +8,44 @@ type
     lastname: string
     age: int
 
-let users: seq[User] = @[
-  User(name: "Katja", lastname: "Kopylevych", age: 32),
-  User(name: "David", lastname: "Krause", age: 32),
-]
+# We use the new UserContext feature of prologue.
+type
+  UserContext = ref object of Context
+    users: seq[User]
 
-proc renderIndex(title: string, users: seq[User]): string {.gcsafe.} =
+method extend(ctx: UserContext) =
+  ctx.users = @[
+    User(name: "Katja", lastname: "Kopylevych", age: 32),
+    User(name: "David", lastname: "Krause", age: 32),
+  ]
+
+proc renderIndex(title: string, users: seq[User]): string =
   compileTemplateFile(getScriptDir() / "index.nwt")
 
-proc renderUser(title: string, idx: int, users: seq[User]): string {.gcsafe.} =
+proc renderUser(title: string, idx: int, users: seq[User]): string =
   let user = users[idx]
   compileTemplateFile(getScriptDir() / "user.nwt")
 
-proc hello*(ctx: Context) {.async, gcsafe.} =
-  resp $renderIndex("someTitle", users)
+proc renderError(title: auto, code: HttpCode, users: seq[User]): string =
+  ## title is `auto` here; nim generics work as well!
+  compileTemplateFile(getScriptDir() / "error.nwt")
 
-proc user*(ctx: Context) {.async, gcsafe.} =
+proc hello*(ctx: UserContext) {.async.} =
+  resp renderIndex("someTitle", ctx.users)
+
+proc user*(ctx: UserContext) {.async.} =
   var idx = 0
   try:
     idx = parseInt(ctx.getPathParams("idx", "0"))
-    resp $renderUser("someTitle", idx, users)
+    resp renderUser("someTitle", idx, ctx.users)
   except:
-    resp "not found", Http404
+    resp "", Http404
+
+proc go404*(ctx: UserContext) {.async.} =
+  resp renderError(title = Http404, code = Http404, ctx.users), Http404
 
 let app = newApp()
 app.get("/", hello)
 app.get("/users/{idx}", user)
-app.run()
+app.registerErrorHandler(Http404, go404)
+app.run(UserContext)
