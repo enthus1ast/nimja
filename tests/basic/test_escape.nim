4e8e8cf5ed9325b6c3740c559fbf21f55345dc53
David Krause <krause@biochem2.uni-frankfurt.de>
2021-08-31 17:10:18 +0200

    enable escape tests
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- tests/basic/test_escape.nim
+++ tests/basic/test_escape.nim
@@ -6,13 +6,11 @@ block:
   proc test(): string = compileTemplateStr("""{{ "{{" }}""")
   doAssert test() == "{{"
 
-## THIS FAILS seems like a lexer issue maybe even #1
-# block:
-#   proc test(): string = compileTemplateStr("""{{ "}}" }} """)
-#   doAssert test() == "}}"
+block:
+  proc test(): string = compileTemplateStr("""{{ "}}" }}""")
+  doAssert test() == "}}"
 
-## THIS FAILS seems like a lexer issue maybe even #1
-# block:
-#   proc test(): string = compileTemplateStr("""{{ "{{}}" }}""")
-#   doAssert test() == "{{}}"
+block:
+  proc test(): string = compileTemplateStr("""{{ "{{}}" }}""")
+  doAssert test() == "{{}}"
 
