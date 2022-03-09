b1b3ed79e2a7e98f61e44c68ccd051eafde49930
David Krause <enthus1ast@users.noreply.github.com>
2022-03-09 11:36:35 +0100

    wip on readme for hcr; hcr example; inc version
    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- examples/hcr/hcrex.nim
+++ examples/hcr/hcrex.nim
@@ -1,110 +1,34 @@
-import jester, std/dynlib, asyncdispatch, osproc, os, times, strformat, strutils
-import print
-
-
+import nimja/hcrutils # Nimja's hot code reloading utilities
+import shared # for the TestObj that is also used in tmpls.nim
+import jester, os
 type
-  Exec* = proc(cw: ChangeWatcher)
-  Paths* = Table[string, Time]
-  ChangeWatcher* = ref object
-    checkTimeout*: int
-    libfile*: string
-    lib*: pointer
-    patterns*: seq[string]
-    paths*: Paths
-    exec*: Exec
+    # You must declare the proc definition from your tmpls.nim here as well.
+    ProcNoParam = proc (): string {.gcsafe, stdcall.}
+    ProcTestObj = proc (to: TestObj): string {.gcsafe, stdcall.}
+    ProcId = proc (id: string): string {.gcsafe, stdcall.}
+    ProcSeq = proc (a: seq[string]): string {.gcsafe, stdcall.}
+    ProcIS = proc (ii: int, ss: string): string {.gcsafe, stdcall.}
 
-template updateFile(path: string) =
-  if not cw.paths.hasKey(path):
-    echo "NEW: ", path
-    cw.paths[path] = getLastModificationTime(path)
-    result.add path
-  else:
-    let cur = getLastModificationTime(path)
-    if cur > cw.paths[path]:
-      echo "CHANGED: ", path
-      cw.paths[path] = cur
-      result.add path
+# We watch the templates folder for change (and also tmpls.nim implicitly)
+var cw = newChangeWatcher(@[getAppDir() / "templates/"])
+asyncCheck cw.recompile() # if a change is detected we recompile tmpls.nim
 
-proc walk(cw: ChangeWatcher): seq[string] =
-  for pattern in cw.patterns:
-    if pattern.fileExists():
-      updateFile(pattern)
-    else:
-      for path in walkDirRec(pattern):
-        updateFile(path)
+var to = TestObj()
+to.foo = "foo!"
 
-proc doRecompile(cw: ChangeWatcher) =
-  echo execShellCmd(fmt"nim c --app:lib {cw.libfile}.nim")
+routes:
+  get "/":
+    resp dyn(ProcNoParam, "index")
 
-proc newChangeWatcher*(patterns: seq[string], exec: Exec, checkTimeout = 1_000, libfile = "tmpls.nim"): ChangeWatcher =
-  result = ChangeWatcher()
-  result.exec = exec
-  result.checkTimeout = checkTimeout
-  result.libfile = splitFile(libfile).name
-  result.patterns = patterns
-  result.patterns.add libfile # always add the libfile
-  result.doRecompile() # compile the lib once on start
-  result.lib = loadLib(DynlibFormat % result.libfile)
+  get "/id/@id":
+    resp dyn(ProcId, "detail", @"id")
 
-proc recompile*(cw: ChangeWatcher) {.async.} =
-  echo "Dynamic recompilation is on; watching:"
-  for pattern in cw.patterns:
-    echo "\t", pattern
-  while true:
-    # echo "tick"
-    var changed = cw.walk()
-    if changed.len > 0:
-      echo "CHANGED .. RECOMPILE"
-      cw.lib.unloadLib()
-      cw.doRecompile()
-      cw.lib = loadLib(DynlibFormat % cw.libfile)
-    await sleepAsync(cw.checkTimeout)
+  get "/error":
+    resp dyn(ProcSeq, "error", @["a", "b", "c"])
 
-# template dyn*(kind, name: untyped, param: typed): string =
-#   # if param.len > 0:
-#   #   cast[kind](cw.lib.symAddr(name))(param)
-#   # else:
-#   #   cast[kind](cw.lib.symAddr(name))()
-#   cast[kind](cw.lib.symAddr(name))(param)
+  get "/about":
+    resp dyn(ProcIS, "about", 1234, "foobaa")
 
-# # template dyn*(kind, name: untyped): string =
-# #   cast[kind](cw.lib.symAddr(name))()
-
-import macros
-
-macro dyn(kind, name: untyped, params: varargs[untyped]): untyped =
-  var kk: string = $kind
-  var nn: string = $name
-  var ps: string = ""
-  for idx, param in params:
-    var pp: string = repr param
-    ps.add pp
-    if idx < params.len - 1:
-      ps.add ", "
-  let cc = fmt"""cast[{kk}](cw.lib.symAddr("{nn}"))({ps})"""
-  return parseStmt(cc)
-
-
-when isMainModule:
-  type
-      ProcNoParam = proc (): string {.gcsafe, stdcall.}
-      ProcId = proc (id: string): string {.gcsafe, stdcall.}
-      ProcSSeq = proc (a: seq[string]): string {.gcsafe, stdcall.}
-      ProcIS = proc (ii: int, ss: string): string {.gcsafe, stdcall.}
-
-  var cw = newChangeWatcher(@[getAppDir() / "templates/"], doRecompile)
-  asyncCheck cw.recompile()
-
-  routes:
-    get "/":
-      resp dyn(ProcNoParam, "index")
-
-    get "/id/@id":
-      resp dyn(ProcId, "detail", @"id")
-
-    get "/error":
-      resp dyn(ProcSSeq, "error", @["a"])
-
-    get "/iss":
-      resp dyn(ProcIS, "iss", 1234, "foobaa")
+  get "/oop":
+    resp dyn(ProcTestObj, "oop", to)
 
--- examples/hcr/tmpls.nim
+++ examples/hcr/tmpls.nim
@@ -1,9 +1,15 @@
+# this file contains you render functions
+# is compiled to a shared lib and loaded by your host application
+# to keep compilation fast, use this file only for templates.
+# this file is also watched by the filewatcher.
+# It can also be changed dynamically!
 import nimja
 import times
 import os
+import shared # for the TestObj
 
 proc index*(): string {.exportc, dynlib.} =
-  var foos =  1355
+  var foos =  1351
   compileTemplateFile(getScriptDir() / "templates/index.nimja")
 
 proc detail*(id: string): string {.exportc, dynlib.} =
@@ -11,30 +17,27 @@ proc detail*(id: string): string {.exportc, dynlib.} =
 
 proc error*(a: seq[string]): string {.exportc, dynlib.} =
   var a = a
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
-  a.add("b")
+  a.add("foo")
   compileTemplateFile(getScriptDir() / "templates/error.nimja")
 
-proc iss*(a: int, b: string): string {.exportc, dynlib.} =
+proc about*(a: int, b: string): string {.exportc, dynlib.} =
   compileTemplateStr("""
   {% extends "templates/partials/_master.nimja" %}
   {% block content %}
+  <h1>About page</h1>
   : {{a}}<br>
   : {{b}}<br>
   {% endblock %}
 
   """)
+
+proc oop*(to: TestObj): string {.exportc, dynlib.} =
+  compileTemplateStr("""
+  {% extends "templates/partials/_master.nimja" %}
+  {% block content %}
+  <h1>TestObj</h1>
+    {{to.foo}}
+  {% endblock %}
+
+  """)
+
