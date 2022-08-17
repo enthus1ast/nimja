03cfe8e6d8b5b074d72a853d5c2c30d47762381a
David Krause <enthus1ast@users.noreply.github.com>
2022-08-17 12:58:46 +0200

    `tmpls` and `tmplf` with context!
    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

---
+++ tests/basic/test_tmplf_with_context.nim
@@ -0,0 +1,28 @@
+discard """
+  joinable: false
+"""
+import ../../src/nimja
+import os
+
+block:
+  type
+    Rax = object
+      aa: string
+      bb: float
+  var rax = Rax(aa: "aaaa", bb: 13.37)
+  var ii = 123
+  doAssert "idx: 123, aa: aaaa, nodes: aaaa, 13.37" ==
+    tmplf(getScriptDir() / "tmplf_with_context.nimja",
+      idx = ii,
+      aa = rax.aa,
+      nodes = rax
+    )
+
+# block:
+#    type
+#      Rax = object
+#        aa: string
+#        bb: float
+#    var rax = Rax(aa: "aaaa", bb: 13.37)
+#    var foo = 123
+#    doAssert "13.37123" == tmpls("""{% if node.aa == "aaaa" %}{{node.bb}}{% endif %}{{baa}}""", node = rax, baa = foo)
---
+++ tests/basic/test_tmpls_with_context.nim
@@ -0,0 +1,38 @@
+discard """
+  joinable: false
+"""
+import ../../src/nimja
+
+block:
+  type
+    Rax = object
+      aa: string
+      bb: float
+  var rax = Rax(aa: "aaaa", bb: 13.37)
+  var ii = 123
+  doAssert "idx: 123, aa: aaaa, nodes: aaaa, 13.37" ==
+    tmpls("idx: {{idx}}, aa: {{aa}}, nodes: {{nodes.aa}}, {{nodes.bb}}",
+      idx = ii,
+      aa = rax.aa,
+      nodes = rax
+    )
+
+block:
+  type
+    Rax = object
+      aa: string
+      bb: float
+  var rax = Rax(aa: "aaaa", bb: 13.37)
+  var foo = 123
+  doAssert "13.37123" == tmpls("""{% if node.aa == "aaaa" %}{{node.bb}}{% endif %}{{baa}}""", node = rax, baa = foo)
+
+block:
+  # test if nimja templates can change stuff
+  type
+    Rax = object
+      aa: string
+      bb: float
+  var rax = Rax(aa: "aaaa", bb: 13.37)
+  var foo = 123
+  doAssert "42.0" == tmpls("""{% if node.aa == "aaaa" %}{%node.bb = 42.0%}{% endif %}{{node.bb}}""", node = rax, baa = foo)
+  doAssert 42.0 == rax.bb
