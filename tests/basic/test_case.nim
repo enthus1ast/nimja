05a418d754d1f0aa03514c05a3214b40c5549d64
David Krause <enthus1ast@users.noreply.github.com>
2022-09-15 10:48:50 +0200

    move case*.nimja templates to "case" folder
    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- tests/basic/test_case.nim
+++ tests/basic/test_case.nim
@@ -8,16 +8,16 @@ import os
 suite "case":
   test "basic test":
     var str = "foo"
-    check "foo" == tmplf(getScriptDir() / "case.nimja")
+    check "foo" == tmplf(getScriptDir() / "case" / "case.nimja")
 
     str = "baa"
-    check "baa" == tmplf(getScriptDir() / "case.nimja")
+    check "baa" == tmplf(getScriptDir() / "case" / "case.nimja")
 
     str = "baz"
-    check "baz" == tmplf(getScriptDir() / "case.nimja")
+    check "baz" == tmplf(getScriptDir() / "case" / "case.nimja")
 
     str = "asdf"
-    check "nothing" == tmplf(getScriptDir() / "case.nimja")
+    check "nothing" == tmplf(getScriptDir() / "case" / "case.nimja")
 
   test "complex test":
     check "AB" == tmpls("""{%- case "a" & "b" -%}{%- of "ab" -%}AB{%endcase%}""")
@@ -27,14 +27,14 @@ suite "case":
       aaa, bbb, ccc, ddd
     var foo: Foo = aaa
     var isNothing: bool
-    check "AAA" == tmplf(getScriptDir() / "case2.nimja", {ee: foo})
-    check "BBB" == tmplf(getScriptDir() / "case2.nimja", {ee: Foo.bbb})
-    check "CCC" == tmplf(getScriptDir() / "case2.nimja", {ee: ccc})
+    check "AAA" == tmplf(getScriptDir() / "case" / "case2.nimja", {ee: foo})
+    check "BBB" == tmplf(getScriptDir() / "case" / "case2.nimja", {ee: Foo.bbb})
+    check "CCC" == tmplf(getScriptDir() / "case" / "case2.nimja", {ee: ccc})
 
     isNothing = true
-    check "nothing" == tmplf(getScriptDir() / "case2.nimja", {ee: ddd})
+    check "nothing" == tmplf(getScriptDir() / "case" / "case2.nimja", {ee: ddd})
 
     isNothing = false
-    check "something" == tmplf(getScriptDir() / "case2.nimja", {ee: ddd})
+    check "something" == tmplf(getScriptDir() / "case" / "case2.nimja", {ee: ddd})
 
 
--- tests/basic/test_case_missing.nim
+++ tests/basic/test_case_missing.nim
@@ -8,4 +8,4 @@ include ../../src/nimja/parser
 type Foo = enum
   aaa, bbb, ccc, ddd
 var foo: Foo = aaa
-discard tmplf(getScriptDir() / "case3.nimja", {ee: ddd})
+discard tmplf(getScriptDir() / "case" / "case3.nimja", {ee: ddd})
